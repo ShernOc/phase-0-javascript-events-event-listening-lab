4d34fb589e426346c4adab6fd122df614bb343ee
Sherlyne <sherlynea1994@hotmail.com>
2024-07-11 23:16:35 +0300

    add event listener to button

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 function addingEventListener() {
+
+function clickAlert(){
+  alert('I was clicked!');
+  }
+
+input.addEventListener('click',clickAlert);
 }
+
+// const input = document.querySelector("#button");// Its also right. 
+const input = document.getElementById ('button');
+
+addingEventListener(); // run it 
+
+
+
+
+
+  
